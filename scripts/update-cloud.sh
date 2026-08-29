ef04ed9490a40edab1c167d33fd22e44a60bc502
Maxim Samsonov <m.samsonov@computer.org>
2026-08-30 00:45:33 +0300

    chore: move Docker Compose ownership from UI to Core

--- scripts/update-cloud.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (C) 2026 Maxim [maxirmx] Samsonov (www.sw.consulting)
-# All rights reserved.
-# This file is a part of the Sarafan application
-
-set -euo pipefail
-
-readonly DEPLOYMENT_TARGET="${1:-${SARAFAN_DEPLOYMENT_TARGET:-production}}"
-exec "$(dirname "$0")/bootstrap-cloud.sh" "$DEPLOYMENT_TARGET"
